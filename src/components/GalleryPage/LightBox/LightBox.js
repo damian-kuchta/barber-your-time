a3400aa168498e311a077a535591b93a98e3efe3
Damian Kuchta <damianes1994@gmail.com>
2022-06-07 18:00:00 +0200

    v. 3.0

--- src/components/GalleryPage/LightBox/LightBox.js
+++ src/components/GalleryPage/LightBox/LightBox.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import classes from "./LightBox.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 const LightBox = ({ children, src, alt, Wrapper = "div", zIndex = 100 }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(()=>{
+    if(isOpen) {
+      document.body.style.overflow = 'hidden';
+    }
+    else {
+      document.body.style.overflow='auto'
+    }
+  }, [isOpen])
+
   const toggleIsOpen = (e) => {
     console.log(e.target);
     if (e.target.localName === "img") {
+      
       setIsOpen(true);
+
     } else {
       setIsOpen(false);
     }
   };
-
   const openImage = (e) => {
     if (e.target.localName === "img") {
       setIsOpen(true);
@@ -38,6 +49,7 @@ const LightBox = ({ children, src, alt, Wrapper = "div", zIndex = 100 }) => {
             left: "0",
             height: "100vh",
             width: "100vw",
+            overflow: "hidden",
             backgroundColor: "rgba(0,0,0,0.7)",
             cursor: "pointer",
             zIndex: "99",
